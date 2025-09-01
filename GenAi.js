d407f4d78535adb4192b0a57d5fa2c0366ddaf66
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-09-01 08:45:49 +0530

    Using LLMs in LangChain

--- GenAi.js
+++ GenAi.js
@@ -1,33 +1,44 @@
+// import "dotenv/config";
+// import { GoogleGenAI } from "@google/genai";
+// import readlineSync from "readline-sync";
+
+// const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+// const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+// // Chat context (memory)
+// const Context = [];
+
+// async function User(userQuery) {
+//   Context.push({
+//     role: "user",
+//     parts: [{ text: userQuery }],
+//   });
+
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents: Context,
+//   });
+//   Context.push({
+//     role: "user",
+//     parts: [{ text: response.text }],
+//   });
+
+//   console.log(response.text);
+// }
+
+// async function main() {
+//   const userQuery = readlineSync.question("Hanji pucho!!");
+//   await User(userQuery); //if i dont write await here the main function will be called early
+//   main();
+// }
+
+// main();
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import "dotenv/config";
-import { GoogleGenAI } from "@google/genai";
-import readlineSync from "readline-sync";
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash",
+  apiKey: process.env.GEMINI_API_KEY,
+});
 
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-
-const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
-
-async function User(userQuery) {
-  Context.push({
-    role: "user",
-    parts: [{ text: userQuery }],
-  });
-
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: Context,
-  });
-  Context.push({
-    role: "user",
-    parts: [{ text: response.text }],
-  });
-
-  console.log(response.text);
-}
-
-async function main() {
-  const userQuery = readlineSync.question("Hanji pucho!!");
-  await User(userQuery); //if i dont write await here the main function will be called early
-  main();
-}
-
-main();
+const response = await model.invoke("the green tea is");
+console.log(response);
