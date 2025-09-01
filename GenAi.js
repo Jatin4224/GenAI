445fce566edacb68a754f70e3ccbe4e92f5838c9
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-09-01 12:28:39 +0530

    > adding  a SystemMessage to give the AI a specific instruction

--- GenAi.js
+++ GenAi.js
@@ -33,17 +33,41 @@
 // }
 
 // main();
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import "dotenv/config";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
-import { HumanMessage } from "@langchain/core/messages";
-
+// 1. Define the LLM
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash",
   apiKey: process.env.GEMINI_API_KEY,
 });
 
-const prompt = [new HumanMessage("What is the capital of Rajasthan?")];
+// 2. Define the Template
+const template = PromptTemplate.fromTemplate(`Answer the question based on the
+context below. If the question cannot be answered using the information
+provided, answer with "I don't know".
 
-const response = await model.invoke(prompt);
-console.log(response);
+Context: {context}
+Question: {question}
+Answer:`);
+
+async function main() {
+  // 3. Fill the Template
+  const prompt = await template.format({
+    context: `Chai is one of the most loved beverages in India.
+    It is made by brewing black tea leaves with milk, water,
+    and spices like ginger and cardamom.
+    Many people enjoy chai in the evening with snacks
+    such as pakoras or biscuits.`,
+
+    question: `Which spice is commonly added to chai for flavor?`,
+  });
+
+  // 4. Invoke the Model
+  const response = await model.invoke(prompt);
+
+  console.log("Gemini says:", response.content);
+}
+
+main();
